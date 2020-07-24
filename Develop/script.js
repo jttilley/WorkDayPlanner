98d879df89e021494333c8ea0e9ee36f57055111
Jason Tilley <jttilley007@yahoo.com>
2020-07-23 23:40:34 -0600

    added psudo

---
+++ Develop/script.js
@@ -0,0 +1,15 @@
+var container = $(".container");
+var curDay = $("#currentDay");
+
+// get the current date and time
+
+// add date to currentday
+
+// function to make an hour segment
+
+// function to color an hour segment
+
+// a click event for adding notes to a segment
+
+// click event for saving a note
+
